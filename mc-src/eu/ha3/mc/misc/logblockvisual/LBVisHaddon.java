501f3904ddd0a4706f2b2bc5a3df2202f92a137f
Hurricaaane <git@ha3.eu>
2014-03-19 21:55:05 +0100

    replace to placeholder comment

--- mc-src/eu/ha3/mc/misc/logblockvisual/LBVisHaddon.java
+++ mc-src/eu/ha3/mc/misc/logblockvisual/LBVisHaddon.java
@@ -14,21 +14,7 @@ import eu.ha3.mc.glitter.Ha3RenderRelay.HRenderEntity;
 import eu.ha3.mc.haddon.SupportsChatEvents;
 import eu.ha3.mc.haddon.SupportsTickEvents;
 
-/*
-            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
-                    Version 2, December 2004 
-
- Copyright (C) 2004 Sam Hocevar <sam@hocevar.net> 
-
- Everyone is permitted to copy and distribute verbatim or modified 
- copies of this license document, and changing it is allowed as long 
- as the name is changed. 
-
-            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
-   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION 
-
-  0. You just DO WHAT THE FUCK YOU WANT TO. 
-*/
+/* xw-placeholder */
 
 public class LBVisHaddon extends HaddonImpl implements SupportsTickEvents, SupportsChatEvents
 {
